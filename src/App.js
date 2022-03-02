22b5186bdac1551abc670f5bef81610544574147
James MacGilp <JOMacgilp@dundee.ac.uk>
2022-03-02 13:17:22 +0000

    rough layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+class Message extends React.Component{
+	render(){
+		return (<div className="Message"> <p> User: {this.props.message.sender} </p>
+						<p> { this.props.message.body } </p>
+				</div>);
+	}
+}
+
+class Thread extends React.Component{
+	constructor(props)
+	{
+		super(props);
+		
+		this.state = { thread: props.thread, isCollapsed: true };
+	}
+	
+	toggleCollapse(){
+		this.setState({isCollapsed: !this.state.isCollapsed});
+	}
+	
+	render(){
+		
+		var msgArr = [];
+	
+		if(!this.state.isCollapsed)
+		{
+			for(var i = 0; i < this.props.thread.messages.length; i++)
+			{
+				msgArr.push(<Message message={this.props.thread.messages[i] } />);
+			}
+		}
+	
+		return (<div> <h2> <button onClick={() => {this.toggleCollapse()}} >+/-</button> {this.state.thread.title} posted by {this.state.thread.sender} </h2>
+			{msgArr} 
+			</div>);
+	}
+}
+
 function App() {
+	const threads = [ { sender: "jim", title: "does this work?", messages: [ { sender: "bob", body: "test1234" }, { sender: "dave", body: "yes, it does work jim!" } ]},
+					  { sender: "susan", title: "What are your hobbies?", messages: [ { sender: "brian", body: "I like swords" }, { sender: "steve", body: "me too" } ]}];
+	
+	var threadArr = [];
+	
+	for(var i = 0; i < threads.length; i++)
+	{
+		threadArr.push(<Thread thread={threads[i] } />);
+	}
+	
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Message Board
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+	  
+	  <div>
+	  {threadArr}
+	  </div>
     </div>
   );
 }
